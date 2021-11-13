fdf9a8f9a364fb06b01f3c05a80a66cf5936795e
Jonatan Cabrera <cabrerajonatan270699@gmail.com>
2021-11-13 19:29:09 -0300

    Editar y eliminar

--- tienda/aplicaciones/productos/views.py
+++ tienda/aplicaciones/productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # modelo
 from aplicaciones.productos.models import Producto
@@ -47,3 +47,32 @@ def GuardarProducto(request):
     return render(request,'productos/agregar_producto.html',{'formulario':agregarproducto})           
             
                     
+def editarproducto(request,pk):
+    producto = Producto.objects.get(pk=pk)
+    
+    if request.method == 'POST':
+        formulario = FormularioProductos(request.POST,instance=producto)
+        
+        #validamos el formulario
+        if formulario.is_valid():
+            formulario_data = formulario.cleaned_data()
+            #obtenemos los datos del formulario
+            nombre = formulario_data.get("nombre")
+            descripcion = formulario_data.get("descripcion")
+            precio = formulario_data.get("precio")
+            stock = formulario_data.get("stock")
+            imangen = formulario_data.get("imagen")
+            # Guardamos la informacion obtenida del formulario en la Base de datos
+            Producto.objects.create(
+                nombre = nombre,
+                descripcion = descripcion,
+                precio = precio,
+                stock = stock,
+                imagen = imangen
+            )
+            return redirect(to = 'producListAll')
+            
+def eliminarsocio(request,pk):
+    producto = Producto.objects.get(pk=pk)
+    producto.delete()
+    return redirect(to = 'producListAll')            
