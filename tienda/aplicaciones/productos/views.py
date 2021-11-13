cf9bd51baf457a7ccf50e5616e51dc51ea6d4f71
Jhon <ztheeban205@gmail.com>
2021-11-13 09:41:51 -0500

    Ver todos los productos

--- tienda/aplicaciones/productos/views.py
+++ tienda/aplicaciones/productos/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 from models import Producto
 
 # Create your views here.
+
+def producListAll(request):
+    """
+    Listamos todos los productos
+    """
+    productos = Producto.objects.all()
+    
+    context = {
+        'productos': productos
+    }
+    return render(request, template_name="productos/nombre_html.html", context=context)
