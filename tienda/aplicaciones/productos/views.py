17ebd076a1f47a508d3b698cc66a833d154a629c
yeysner <yeinerguerrero1991@gmail.com>
2021-11-13 09:00:44 -0500

    Formulario de productos y otros
    Creacion del formulario de productos, agregue el formulario a la views de producto, cree la validación del formulario, cree la instancia para pasar los datos del formulario al modelo y la base de datos.
    como las aplicaciones estaban en otra carpeta, modifique el archivo app de productos y usuarios, modifique el arcivo admin para la correcta importacion del modelo producto.

--- tienda/aplicaciones/productos/views.py
+++ tienda/aplicaciones/productos/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-
+from .forms import FormularioProductos
+from aplicaciones.productos.models import Producto
 # modelo
-from models import Producto
+
 
 # Create your views here.
+
+def VistaProducto(request):
+    
+    
+    # Creamos una istancia para el formulario
+    if request.method == 'POST':
+        formulario = FormularioProductos(request.POST)
+        
+        #validamos el formulario
+        if formulario.is_valid():
+            formulario_data = formulario.cleaned_data()
+            #obtenemos los datos del formulario
+            nombre = formulario_data.get("nombre")
+            descripcion = formulario_data.get("descripcion")
+            precio = formulario_data.get("precio")
+            stock = formulario_data.get("stock")
+            imangen = formulario_data.get("imagen")
+            # Guardamos la informacion obtenida del formulario en la Base de datos
+            Producto.objects.create(
+                nombre = nombre,
+                descripcion = descripcion,
+                precio = precio,
+                stock = stock,
+                imagen = imangen
+            )
+            
+            
+                    
